18e16a036a7fc02668d297ab4b51eeb62e40a888
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-08 18:27:34 -0300

    chore: Routes

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const sellers = [
-  'Landix',
-  'Vitor'
-]
-
-app.get('/sellers', (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredSellers = search ? sellers.filter(seller => seller.includes(search)) : sellers;
-
-  return response.json(filteredSellers);
-});
-
-app.get('/sellers/:id', (resquest, response) => {
-  const id = Number(resquest.params.id);
-
-  const seller = sellers[id];
-
-  return response.json(seller);
-})
-
-app.post('/sellers', (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-
-  const seller = {
-    name: data.name,
-    email: data.email
-  };
-
-  return response.json(seller);
-})
+app.use(routes);
 
 app.listen(3333);
