44c441740ed0adf2b5afba91e1bdb6fdda50d3fe
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-08 17:40:05 -0300

    chore: Seller route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,41 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/seller', (request, response) => {
-  console.log('Listagem de vendedores Landix');
+app.use(express.json());
 
-  response.json([
-    'Landix',
-  ])
+const sellers = [
+  'Landix',
+  'Vitor'
+]
+
+app.get('/sellers', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredSellers = search ? sellers.filter(seller => seller.includes(search)) : sellers;
+
+  return response.json(filteredSellers);
 });
+
+app.get('/sellers/:id', (resquest, response) => {
+  const id = Number(resquest.params.id);
+
+  const seller = sellers[id];
+
+  return response.json(seller);
+})
+
+app.post('/sellers', (request, response) => {
+  const data = request.body;
+
+  console.log(data);
+
+  const seller = {
+    name: data.name,
+    email: data.email
+  };
+
+  return response.json(seller);
+})
 
 app.listen(3333);
