aea4a43d980b1146d7b5562294ea5638267045d4
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-10 18:38:14 -0300

    chore: Saving Seller and Client on DB

--- src/database/migrations/01_create_clients.ts
+++ src/database/migrations/01_create_clients.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex'
 export async function up(knex: Knex) {
   return knex.schema.createTable('clientes', table => {
     table.increments('cdcl').primary();
-    table.string('dsnome', 50).notNullable();
+    table.string('clnome', 50).notNullable();
     table.decimal('dslim', 2).notNullable();
 
     table.specificType('idtipo', 'char(2)')
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,42 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Landix' });
-})
+interface ClientItem  {
+  clnome: string;
+  dslim: number;
+  idtipo: string
+}
+
+routes.post('/sellers', async (request, response) => {
+  const {
+    dsnome,
+    cdtab,
+    dtnasc,
+    cliente,
+  } = request.body;
+
+  const insertedSellersIds = await db('vendedores').insert({
+    dsnome,
+    cdtab,
+    dtnasc,
+  })
+
+  const vendedores_cdvend = insertedSellersIds[0];
+
+  const clienteDoVendedor = cliente.map((clientItem: ClientItem) => {
+    return {
+      vendedores_cdvend,
+      clnome: clientItem.clnome,
+      dslim: clientItem.dslim,
+      idtipo: clientItem.idtipo,
+    };
+  })
+
+  await db('clientes').insert(clienteDoVendedor);
+
+  return response.send();
+});
 
 export default routes;
